5f93611842edc1c2e7b3196ce67776f964acc46e
Your Name <dmitriy.murashov@autimation-engineering.ru>
2016-04-27 02:00:57 -0700

    change to twisted async work with serial and looping calls

--- TestVelocityDirectionGPS.py
+++ TestVelocityDirectionGPS.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
+import os
 import serial
 import serial.tools.list_ports
 import time
 import sys
 import math
+import RPi.GPIO as GPIO
+import pynmea2
 
-
+from twisted.internet.task import LoopingCall
+from twisted.internet import reactor
 
 
 def convertGPSToXYZ(geoPoint):
@@ -120,43 +124,83 @@ def verifyVDB(inputLine, geoPoint):
     return True
 
 
+#---------------------------------------------------------------#
+# Globals vars
+#---------------------------------------------------------------#
+timeDelay = 0
+theTotalDistance = 0.0
+theTotalDistanceToday = 0.0
+counter = 1
+theGPSDevice = serial.Serial()
+previousGeoPoint = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+currentGeoPoint = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+previousTime = previousGeoPoint[2]
+theVelocity = previousGeoPoint[3] * 1.150777
+theBearing = previousGeoPoint[4]
+theDate = previousGeoPoint[5]
+previousDate = theDate
+theTotalDistance = 0.0
+theTotalDistanceToday = 0.0
+currentTime = 0.0
+previousTime = currentTime
 
 
-#  make sure you first created the directory 'C:\\GPS\\'
-#  Start of the main program
-# set 10 seconds of time delay
-timeDelay = 10
 
+def Init():
+ global timeDelay
+ global theTotalDistance
+ global theTotalDistanceToday
+ global counter
+ global theGPSDevice
+ global previousGeoPoint
+ global currentGeoPoint
+ global previousTime
+ global theVelocity
+ global theBearing
+ global theDate
+ global previousDate
+ global theTotalDistance
+ global theTotalDistanceToday
+ global currentTime
+ global previousTime
+#----------------------------------------------------------------#
+# Init Serial port
+#----------------------------------------------------------------#
+ timeDelay = 10
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(2, GPIO.IN)
 
-thePortNumber = ''  # hopefully this will become non-empty later
-listOfCOMPorts = list(serial.tools.list_ports.comports())
-#print("%s"%listOfCOMPorts)
-for i in range(len(listOfCOMPorts)):
-#    print( listOfCOMPorts[i][1][0:12])
+ thePortNumber = ''  # hopefully this will become non-empty later
+ listOfCOMPorts = list(serial.tools.list_ports.comports())
+ for i in range(len(listOfCOMPorts)):
     if listOfCOMPorts[i][1][0:12] == 'USB-Serial C':
       thePortNumber = listOfCOMPorts[i][0]      # get the active/used port number
-if thePortNumber == '':
+ if thePortNumber == '':
     print('Sorry but the GPS device is not plugged in')
     sys.exit()
-#serial.Win32Serial     # if you get here, thePortNumber should be valid, so we should set the COM parameters required for reading
-theGPSDevice = serial.Serial(port=thePortNumber, baudrate=4800, bytesize=8, stopbits=1, parity='N', xonxoff=False, timeout=None)
-print(thePortNumber)
+ theGPSDevice = serial.Serial(port=thePortNumber, baudrate=4800, bytesize=8, stopbits=1, parity='N', xonxoff=False, timeout=0)
+ print(thePortNumber)
 
 
-GoodStart = False
-while (not GoodStart):
-    inputSentence = theGPSDevice.readline()
-    print(inputSentence)
-#    inputLine = "".join(map(chr, inputSentence))
-    inputLine = inputSentence
-    previousGeoPoint = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]     # Latitude  Longitude  Time  Velocity  Bearing  Date
-    if verifyVDB(inputLine, previousGeoPoint):
-        previousTime = previousGeoPoint[2]
-        theVelocity = previousGeoPoint[3] * 1.150777
-        theBearing = previousGeoPoint[4]
-        theDate = previousGeoPoint[5]
-        previousDate = theDate
-        GoodStart = True
+
+#---------------------------------------------------------------#
+# Start GPS positioning procedure
+#---------------------------------------------------------------#
+# GoodStart = False
+# while (not GoodStart):
+#    inputSentence = theGPSDevice.readline()
+#    print(inputSentence)
+#    inputLine = inputSentence
+#    previousGeoPoint = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]     # Latitude  Longitude  Time  Velocity  Bearing  Date
+#    if verifyVDB(inputLine, previousGeoPoint):
+#        previousTime = previousGeoPoint[2]
+#        theVelocity = previousGeoPoint[3] * 1.150777
+#        theBearing = previousGeoPoint[4]
+#        theDate = previousGeoPoint[5]
+#        previousDate = theDate
+#        GoodStart = True
+
 
 
 theTotalDistance = 0.0
@@ -165,24 +209,48 @@ counter = 1
 headerLine = 'Count:' + "," + 'Date (dd/mm/yyyy):' + "," + 'Time (decimal seconds):' + "," + 'Latitude (decimal degrees):' + "," + 'Longitude (decimal degrees):' + "," + 'Velocity (miles per hour):' + "," + 'Current Bearing (degrees):' + "," + 'Total Distance (miles):' + "," + 'Total Distance Today (miles):' + chr(13) + chr(10)
 outputFile = open('LoggedData.csv', 'a')  # open log file
 outputFile.write(headerLine)
-outputLine = str(counter) + "," + str(theDate) + "," + str(previousTime) + "," + str(previousGeoPoint[0]) + "," + str(previousGeoPoint[1]) + "," + format(theVelocity, '.1f') + "," + format(theBearing, '.1f') + "," + format(theTotalDistance, '.1f') + "," + format(theTotalDistanceToday, '.1f') + chr(13) + chr(10)
-outputFile.write(outputLine)
+#outputLine = str(counter) + "," + str(theDate) + "," + str(previousTime) + "," + str(previousGeoPoint[0]) + "," + str(previousGeoPoint[1]) + "," + format(theVelocity, '.1f') + "," + format(theBearing, '.1f') + "," + format(theTotalDistance, '.1f') + "," + format(theTotalDistanceToday, '.1f') + chr(13) + chr(10)
+#outputFile.write(outputLine)
 outputFile.close()
-print('Counter = ' + str(counter))
-counter = counter + 1
+#print('Counter = ' + str(counter))
+#counter = counter + 1
 
-while True:                                        # loop forever
-    inputSentence = theGPSDevice.readline()        # read in a sentence worth of active data
-#    inputLine = "".join(map(chr, inputSentence))   # convert the byte array to a string of Unicode characters
+reader = pynmea2.NMEAStreamReader()
+data = ""
 
-    inputLine = inputSentence
+def LoopProc():
+ global timeDelay
+ global theTotalDistance
+ global theTotalDistanceToday
+ global counter
+ global theGPSDevice
+ global previousGeoPoint
+ global currentGeoPoint
+ global previousTime
+ global theVelocity
+ global theBearing
+ global theDate
+ global previousDate
+ global theTotalDistance
+ global theTotalDistanceToday
+ global currentTime
+ global previousTime
+ global reader
+ global data
+ 
+ data = theGPSDevice.read()
+
+ try:
+  for msg in reader.next(data):
+    print(msg)
+    inputLine = str(msg) + "  "
     currentGeoPoint = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]    # Latitude  Longitude  Time  Velocity  Bearing  Date
     if verifyVDB(inputLine, currentGeoPoint):
         currentTime = currentGeoPoint[2]
         theVelocity = previousGeoPoint[3] * 1.150777    # convert knots to miles per hour
         theBearing = previousGeoPoint[4]
-        theDate = previousGeoPoint[5]
-        if ( (currentTime - previousTime) > (8 * timeDelay // 9) ) and ( (currentGeoPoint[0] != previousGeoPoint[0]) or (currentGeoPoint[1] != previousGeoPoint[1]) ):
+        theDate = currentGeoPoint[5]
+        if ( (currentTime - previousTime) > timeDelay  ) and ( (currentGeoPoint[0] != previousGeoPoint[0]) or (currentGeoPoint[1] != previousGeoPoint[1]) ):
             theDistanceChange = distanceTravelled(previousGeoPoint, currentGeoPoint)
             if (theVelocity == 0.0):
                 theDistanceChange =0.0;
@@ -193,6 +261,7 @@ while True:                                        # loop forever
             theTotalDistance = theTotalDistance + theDistanceChange
             previousGeoPoint[0] = currentGeoPoint[0]
             previousGeoPoint[1] = currentGeoPoint[1]
+            previousGeoPoint = currentGeoPoint
             previousTime = currentTime
             previousDate = theDate
             outputLine = str(counter) + "," + str(theDate) + "," + str(currentTime) + "," + str(currentGeoPoint[0]) + "," + str(currentGeoPoint[1]) + "," + format(theVelocity, '.1f') + "," + format(theBearing, '.1f') + "," + format(theTotalDistance, '.1f') + "," + format(theTotalDistanceToday, '.1f') + chr(13) + chr(10)
@@ -205,8 +274,49 @@ while True:                                        # loop forever
             outputFile.close()                                 # keep the file closed, except when writing to it!
             print("Counter = " + str(counter))                 # print the value of the counter            
             counter = counter + 1                              # increment the counter only when we have a valid sentence
-            time.sleep(timeDelay)                              # wait for 10 seconds before looping
+	    return 1
 
-# note that if we don't get a valid RMC sentence, then go back and do another read until we get a valid RMC sentence
+ except pynmea2.nmea.ParseError:
+  print ("Can't parse");
 
-# the above infinite loop and thus the entire program can be halted at any time by pressing CTRL+F6
+
+def Check_WiFi():
+  hostname = "www.rcofox.com"
+  response = os.system("ping -c 1 " + hostname)
+  if response == 0:
+    pingstatus = "Network Active"
+    return True
+  else:
+    pingstatus = "Network Error"
+    return False
+
+def Power_Off():
+  response = os.system("sudo shutdown -h now")
+
+def Cloud_Loop():
+ return True
+
+Stop_Counter = 0 
+
+def Timers():
+ global Stop_Counter
+ global theVelocity
+ print("Timer loop")
+ if theVelocity < 1 and not GPIO.input(2):
+	Stop_Counter = Stop_Counter + 1
+ else:
+	Stop_Counter = 0
+ return True
+
+Init()
+
+Loopc = LoopingCall(LoopProc)
+Loopc.start(0.001)
+
+Loop_cloud = LoopingCall(Cloud_Loop)
+Loop_cloud.start(20)
+
+Loop_timers = LoopingCall(Timers)
+Loop_timers.start(1)
+
+reactor.run()
